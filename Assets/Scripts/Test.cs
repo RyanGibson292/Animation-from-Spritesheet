df2cc8e35576decd56ba4206ef2ba2f956b4e574
RyanGibson292 <72360102+RyanGibson292@users.noreply.github.com>
2020-11-02 16:24:44 +0000

    Initial commit

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+    private Animator controller;
+    private Rigidbody2D body;
+
+    void Start() {
+        controller = GetComponent<Animator>();
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    void Update() {
+        DoMovement();
+    }
+
+    private void DoMovement() {
+        if(Input.GetKey("left") || Input.GetKey("a")) {
+            controller.SetBool("IsWalkingRight", false);
+            controller.SetBool("IsWalkingLeft", true);
+        } else if(Input.GetKey("right") || Input.GetKey("d")) {
+            controller.SetBool("IsWalkingLeft", false);
+            controller.SetBool("IsWalkingRight", true);
+        } else {
+            controller.SetBool("IsWalkingLeft", false);
+            controller.SetBool("IsWalkingRight", false);
+            controller.SetBool("IsIdle", true);
+        }
+    }
+}
