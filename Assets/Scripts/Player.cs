103698f34fb17deb4de76e599de86e3f09ec4eab
RyanGibson292 <72360102+RyanGibson292@users.noreply.github.com>
2020-11-04 16:05:19 +0000

    8 directions, crossfading

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,7 +6,7 @@ public class Player : MonoBehaviour {
     private Animator controller;
     private Rigidbody2D body;
 
-    private const string LEFT = "WalkLeft", RIGHT = "WalkRight", BACK = "WalkBack", FORWARD = "WalkForward", SPIN = "Spin";
+    private const string LEFT = "WalkLeft", RIGHT = "WalkRight", BACK = "WalkBack", FORWARD = "WalkForward", SPIN = "Spin", BACK_LEFT = "BackLeft", BACK_RIGHT = "BackRight", UP_RIGHT = "ForwardRight", UP_LEFT = "ForwardLeft";
 
     void Start() {
         controller = GetComponent<Animator>();
@@ -19,17 +19,41 @@ public class Player : MonoBehaviour {
 
     private void DoMovement() {
         Vector2 velocity = body.velocity; // Create a duplicate velocity so it can be manually adjusted
-        if(LeftPressed()) {
-            controller.Play(LEFT);
+        if(LeftPressed() && !(UpPressed() || DownPressed())) {
+            controller.CrossFade(LEFT, 2f);
+            controller.Update(2f);
             velocity.x = -5f;
-        } else if(RightPressed()) {
-            controller.Play(RIGHT);
+        } else if(RightPressed() && !(UpPressed() || DownPressed())) {
+            controller.CrossFade(RIGHT, 2f);
+            controller.Update(2f);
             velocity.x = 5f;
-        } else if(UpPressed()) {
-            controller.Play(BACK);
+        } else if(UpPressed() && !(LeftPressed() || RightPressed())) {
+            controller.CrossFade(BACK, 2f);
+            controller.Update(2f);
             velocity.y = 5f;
-        }  else if(DownPressed()) {
-            controller.Play(FORWARD);
+        }  else if(DownPressed() && !(LeftPressed() || RightPressed())) {
+            controller.CrossFade(FORWARD, 2f);
+            controller.Update(2f);
+            velocity.y = -5f;
+        } else if(LeftPressed() && UpPressed()) {
+            controller.CrossFade(BACK_LEFT, 2f);
+            controller.Update(2f);
+            velocity.x = -5f;
+            velocity.y = 5f;
+        } else if(RightPressed() && UpPressed()) {
+            controller.CrossFade(BACK_RIGHT, 2f);
+            controller.Update(2f);
+            velocity.x = 5f;
+            velocity.y = 5f;
+        } else if(RightPressed() && DownPressed()) {
+            controller.CrossFade(UP_RIGHT, 2f);
+            controller.Update(2f);
+            velocity.x = 5f;
+            velocity.y = -5f;
+        } else if(LeftPressed() && DownPressed()) {
+            controller.CrossFade(UP_LEFT, 2f);
+            controller.Update(2f);
+            velocity.x = -5f;
             velocity.y = -5f;
         } else if(ShiftPressed()) {
             controller.Play(SPIN);
